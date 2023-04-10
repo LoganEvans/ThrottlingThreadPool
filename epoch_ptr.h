9cf20934fb51246c08a1a06a81e3efaade825e2d
Logan Evans <loganpevans@gmail.com>
2023-04-09 17:23:54 -0700

    Rough draft of Link::remove.

--- epoch_ptr.h
+++ epoch_ptr.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <functional>
+
 #include "HyperSharedPointer.h"
 
+template <typename T>
 class EpochPtr {
  public:
+  T* get() const;
+  void new_epoch();
+  void defer(std::function<void()> func);
+
+ private:
 };
 
