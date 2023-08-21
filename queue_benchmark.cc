6ee30bdf353dc285c9f2da17742bdfc0d3a64d12
Logan Evans <loganpevans@gmail.com>
2023-08-20 19:33:15 -0700

    Use common benchmark code

--- queue_benchmark.cc
+++ queue_benchmark.cc
@@ -8,191 +8,158 @@
 
 namespace theta {
 
-template <typename QType>
-static void BM_multi_producer_single_consumer(benchmark::State &state) {
+template <typename QType, bool kUseTry>
+static void producer_consumer(benchmark::State& state, int num_producers,
+                              int num_consumers) {
   QType queue{QueueOpts{}};
-  std::atomic<bool> done{false};
 
-  auto work = [&]() {
-    int foo;
-    while (!done.load(std::memory_order::acquire)) {
-      queue.try_push(&foo);
+  std::atomic<bool> done{false};
+  int end_sentinel;
+  std::mutex mu;
+
+  auto consumer_work = [&]() {
+    while (true) {
+      int* x = nullptr;
+      if constexpr (kUseTry) {
+        while (true) {
+          auto res = queue.try_pop();
+          if (res.has_value()) {
+            x = res.value();
+            break;
+          }
+          std::this_thread::yield();
+        }
+      } else {
+        x = queue.pop();
+      }
+      if (x == &end_sentinel) {
+        return;
+      }
+    }
+  };
+
+  std::vector<std::thread> consumers;
+  for (int64_t i = 0; i < num_consumers; i++) {
+    consumers.push_back(std::thread{consumer_work});
+  }
+
+  auto producer_work = [&]() {
+    const size_t kBatchSize = 10000;
+    while (true) {
+      {
+        std::lock_guard l{mu};
+        if (done.load(std::memory_order::acquire) ||
+            !state.KeepRunningBatch(kBatchSize)) {
+          done.store(true, std::memory_order::release);
+          return;
+        }
+      }
+
+      int foo;
+      for (size_t i = 0; i < kBatchSize; i++) {
+        if constexpr (kUseTry) {
+          while (!queue.try_push(&foo)) {
+            std::this_thread::yield();
+          }
+        } else {
+          queue.push(&foo);
+        }
+      }
     }
   };
 
   std::vector<std::thread> producers;
-  for (int64_t i = 0; i < state.range(0); i++) {
-    producers.push_back(std::thread{work});
+  for (int64_t i = 0; i < num_producers; i++) {
+    producers.push_back(std::thread{producer_work});
   }
-
-  for (auto _ : state) {
-    queue.try_pop();
-  }
-
-  done.store(true, std::memory_order::release);
 
   for (auto& p : producers) {
     p.join();
   }
+
+  for (int i = 0; i < num_consumers; i++) {
+    queue.push(&end_sentinel);
+  }
+
+  for (auto& p : consumers) {
+    p.join();
+  }
 }
 
-//BENCHMARK_TEMPLATE(BM_multi_producer_single_consumer, MPSCQueue<int*>)
-//    ->Args({1})
-//    ->Args({2})
-//    ->Args({4})
-//    ->Args({8})
-//    ->Args({12})
-//    ->Args({24});
+template <typename QType>
+static void BM_multi_producer_single_consumer(benchmark::State& state) {
+  producer_consumer<QType, /*kUseTry=*/false>(state, state.range(0), 1);
+}
+// BENCHMARK_TEMPLATE(BM_multi_producer_single_consumer, MPSCQueue<int*>)
+//     ->Args({1})
+//     ->Args({2})
+//     ->Args({4})
+//     ->Args({8})
+//     ->Args({12})
+//     ->Args({24});
 BENCHMARK_TEMPLATE(BM_multi_producer_single_consumer, Queue<int*>)
     ->Args({1})
     ->Args({2})
     ->Args({4})
     ->Args({8})
     ->Args({12})
-    ->Args({24})
-    ;
+    ->Args({24});
 
 template <typename QType>
-static void BM_multi_producer_multi_consumer_try(benchmark::State &state) {
-  QType queue{QueueOpts{}};
-
-  std::atomic<bool> done{false};
-  int end_sentinel;
-  std::mutex mu;
-
-  auto consumer_work = [&]() {
-    while (true) {
-      auto x = queue.try_pop();
-      if (x == &end_sentinel) {
-        return;
-      }
-    }
-  };
-
-  std::vector<std::thread> consumers;
-  for (int64_t i = 0; i < state.range(0); i++) {
-    consumers.push_back(std::thread{consumer_work});
-  }
-
-  auto producer_work = [&]() {
-    const size_t kBatchSize = 10000;
-    while (true) {
-      {
-        std::lock_guard l{mu};
-        if (done.load(std::memory_order::acquire) ||
-            !state.KeepRunningBatch(kBatchSize)) {
-          done.store(true, std::memory_order::release);
-          while (!queue.try_push(&end_sentinel)) {
-          }
-          return;
-        }
-      }
-
-      int foo;
-      for (size_t i = 0; i < kBatchSize; i++) {
-        queue.try_push(&foo);
-      }
-    }
-  };
-
-  std::vector<std::thread> producers;
-  for (int64_t i = 0; i < state.range(0); i++) {
-    producers.push_back(std::thread{producer_work});
-  }
-
-  for (auto& p : producers) {
-    p.join();
-  }
-
-  for (auto& p : consumers) {
-    p.join();
-  }
+static void BM_multi_producer_single_consumer_try(benchmark::State& state) {
+  producer_consumer<QType, /*kUseTry=*/true>(state, state.range(0), 1);
 }
+// BENCHMARK_TEMPLATE(BM_multi_producer_single_consumer_try, MPSCQueue<int*>)
+//     ->Args({1})
+//     ->Args({2})
+//     ->Args({4})
+//     ->Args({8})
+//     ->Args({12})
+//     ->Args({24});
+BENCHMARK_TEMPLATE(BM_multi_producer_single_consumer_try, Queue<int*>)
+    ->Args({1})
+    ->Args({2})
+    ->Args({4})
+    ->Args({8})
+    ->Args({12})
+    ->Args({24});
 
-//BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer_try, MPSCQueue<int*>)
-//    ->Args({1})
-//    ->Args({2})
-//    ->Args({4})
-//    ->Args({6})
-//    ->Args({8})
-//    ->Args({12})
-//    ->Args({24});
+template <typename QType>
+static void BM_multi_producer_multi_consumer_try(benchmark::State& state) {
+  producer_consumer<QType, /*kUseTry=*/true>(state, state.range(0),
+                                             state.range(0));
+}
+// BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer_try, MPSCQueue<int*>)
+//     ->Args({1})
+//     ->Args({2})
+//     ->Args({4})
+//     ->Args({6})
+//     ->Args({8})
+//     ->Args({12})
+//     ->Args({24});
 //
-//BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer_try, Queue<int*>)
-//    ->Args({1})
-//    ->Args({2})
-//    ->Args({4})
-//    ->Args({6})
-//    ->Args({8})
-//    ->Args({12})
-//    ->Args({24})
-//    ;
+BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer_try, Queue<int*>)
+    ->Args({1})
+    ->Args({2})
+    ->Args({4})
+    ->Args({6})
+    ->Args({8})
+    ->Args({12})
+    ->Args({24});
 
 template <typename QType>
-static void BM_multi_producer_multi_consumer(benchmark::State &state) {
-  QType queue{QueueOpts{}};
-
-  std::atomic<bool> done{false};
-  int end_sentinel;
-  std::mutex mu;
-
-  auto consumer_work = [&]() {
-    while (true) {
-      auto x = queue.pop();
-      if (x == &end_sentinel) {
-        return;
-      }
-    }
-  };
-
-  std::vector<std::thread> consumers;
-  for (int64_t i = 0; i < state.range(0); i++) {
-    consumers.push_back(std::thread{consumer_work});
-  }
-
-  auto producer_work = [&]() {
-    const size_t kBatchSize = 10000;
-    while (true) {
-      {
-        std::lock_guard l{mu};
-        if (done.load(std::memory_order::acquire) ||
-            !state.KeepRunningBatch(kBatchSize)) {
-          done.store(true, std::memory_order::release);
-          queue.push(&end_sentinel);
-          return;
-        }
-      }
-
-      int foo;
-      for (size_t i = 0; i < kBatchSize; i++) {
-        queue.push(&foo);
-      }
-    }
-  };
-
-  std::vector<std::thread> producers;
-  for (int64_t i = 0; i < state.range(0); i++) {
-    producers.push_back(std::thread{producer_work});
-  }
-
-  for (auto& p : producers) {
-    p.join();
-  }
-
-  for (auto& p : consumers) {
-    p.join();
-  }
+static void BM_multi_producer_multi_consumer(benchmark::State& state) {
+  producer_consumer<QType, /*kUseTry=*/false>(state, state.range(0),
+                                              state.range(0));
 }
-
-//BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer, Queue<int*>)
-//    ->Args({1})
-//    ->Args({2})
-//    ->Args({4})
-//    ->Args({6})
-//    ->Args({8})
-//    ->Args({12})
-//    ->Args({24})
-//    ;
+BENCHMARK_TEMPLATE(BM_multi_producer_multi_consumer, Queue<int*>)
+    ->Args({1})
+    ->Args({2})
+    ->Args({4})
+    ->Args({6})
+    ->Args({8})
+    ->Args({12})
+    ->Args({24});
 
 }  // namespace theta
 
